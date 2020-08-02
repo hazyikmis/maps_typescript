6f7ac513ad5dd4fe7bf76b7b4515156f23e26755
Halil Azyikmis <hazyikmis@yahoo.com>
2020-08-02 02:39:37 +0200

    showing company and user on the google map

---
+++ src/CustomMap.ts
@@ -0,0 +1,62 @@
+// import { User } from "./User";
+// import { Company } from "./Company";
+
+//instructions to other classes on how they can be an argument to addMarker
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 2,
+      zoomControl: true,
+      center: { lat: 0, lng: 0 },
+    });
+  }
+
+  /* BAD APPROACH
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      // position: {
+      //   lat: user.location.lat,
+      //   lng: user.location.lng,
+      // },
+      position: user.location,
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: company.location,
+    });
+  }
+*/
+
+  // NOT TOO BAD APPROACH, what about if we have more classes having "location": Park.ts, CarLot.ts, etc...
+  // In this case, we need to import all these classes up above and also refer to here
+  /*
+  addMarker(mappable: User | Company): void {
+    //mappable.___ //only common property is location
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: mappable.location,
+    });
+  }
+  */
+
+  addMarker(mappable: Mappable): void {
+    //mappable.___ //only common property is location
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: mappable.location,
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hi there");
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+
+const user = new User();
+console.log(user);
+const company = new Company();
+console.log(company);
+
+/*
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 2,
+  zoomControl: true,
+  center: { lat: 0, lng: 0 },
+});
+*/
+
+const customMap = new CustomMap("map");
+/*
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
+*/
+customMap.addMarker(user);
+customMap.addMarker(company);
